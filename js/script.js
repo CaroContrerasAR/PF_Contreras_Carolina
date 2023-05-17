47ec4831797df4e07634df1ffcd55e8289c1bcf0
Carolina Contreras <acc_carolina@hotmail.com>
2023-05-17 00:29:31 -0300

    grabando2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-let cotizacionD = 0
-let cotizacionP = 0
-let compro = 0
-let percep = asignarOperacion("percepcion")
-let imp = asignarOperacion("imppais")
+// let cotizacionD = 0
+// let cotizacionP = 0
+// let compro = 0
+// let percep = asignarOperacion("percepcion")
+// let imp = asignarOperacion("imppais")
 
 class Monedas {
   constructor(id, nombre, signo, compra, venta) {
@@ -27,24 +27,89 @@ moneda.push(new Monedas (5, "Reales", "r$", 41.15, 43.75));
 let formulario = document.getElementsByName("form")
 let cantidad = document.getElementById("formCantidad")
 let divisa1 = document.getElementById("formDivisas1")
-let boton = document.getElementById("formBtn")
 let divisa2 = document.getElementById("formDivisas2")
+let boton = document.getElementById("formBtn")
+let exc = document.getElementById("exchange");
+let exc1 = document.getElementById("exchange");
+let exc2 = document.getElementById("exchange");
+
+// Acá llamo a la función que esta mas abajo
+mostrarMonedas()
+
+// Funciones
+function mostrarMonedas(){
+  moneda.forEach((moneda) => {
+    const option = document.createElement("option")
+    option.text = moneda.nombre
+    option.value = moneda.compra
+    const nameD1 = moneda.nombre
+    divisa1.appendChild(option)
+  })
+  moneda.forEach((moneda) => {
+    const option = document.createElement("option")
+    option.text = moneda.nombre
+    option.value = moneda.compra
+    const nameD2 = moneda.nombre
+    divisa2.appendChild(option)
+  })
+}
+ 
+cantidad.onchange = (e) => {
+  e.preventDefault();
+  console.log("change cantidad");
+  calcular(divisa1, divisa2);
+}
+
+divisa1.onchange = (e) => {
+  e.preventDefault();
+  console.log("change div1");
+  calcular(divisa1, divisa2);
+  if (divisa1.text == "Pesos") {
+    exc.innerHTML = `<p> El Cambio es <strong>${resultado.toFixed(4)}</strong>\n recuerde agregar los Impuestos sobre la operacion:\n 35% de Percepcion RG4815/20 son $ ${percep(cantidad.value)}\n 30% de Impuesto Pais son $ ${imp(cantidad.value)}</p>`;
+  }
+}
+
+divisa2.onchange = (e) => {
+  e.preventDefault();
+  console.log("change div2");
+  calcular(divisa1, divisa2);
+}
+
+const calcular = (divisa1, divisa2) => {
+  console.log("calculo");
+  let resultado = cantidad.value * (divisa1.value / divisa2.value);
+  console.log(divisa1.text)
+  console.log(divisa2.text)
+  exc.innerHTML = `<p> El Cambio ${cantidad.value} ${divisa1.text} es <strong>${resultado.toFixed(4)} ${divisa2.text}</strong></p>`;
+}
+//   if(divisa1.text!="Pesos"){
+//   exc.innerHTML = `<p> El Cambio es <strong>${(resultado).toFixed(4)}</strong></p>`
+//   }else{
+//     exc.innerHTML = `<p> El Cambio es <strong>${(resultado).toFixed(4)}</strong>\n recuerde agregar los Impuestos sobre la operacion:\n 35% de Percepcion RG4815/20 son $ ${percep(cantidad.value)}\n 30% de Impuesto Pais son $ ${imp(cantidad.value)}</p>`
+//   }
+// }
+
 
 //Defino los Eventos de Boton
-boton.addEventListener('click', respuestaClick)
+boton.addEventListener('click', (e) =>{
+  e.preventDefault();
+
+  respuestaClick()})
 
 // Con esto capturo el div del html donde se van a mostrar la convesion de divisas  (puede ser por el ID, class, name)
 let contenedor = document.getElementById("contenedor");
 
-// Acá llamo a la función que esta mas abajo
-mostrar(moneda);
+
+
 
 // functions
 function respuestaClick(){
   temp=divisa1.value
   divisa1.value=divisa2.value
   divisa2.value=temp
+  calcular(divisa1,divisa2)
 }
+
 function validar(){
   if(cantidad.value > 0){
     respuestaClick()
@@ -52,35 +117,21 @@ function validar(){
   }else{contenedor.innerHTML ="la cantidad debe ser mayor que cero"}
 }
 
-function mostrar(moneda) {
-  cantidad.onchange=()=>{contenedor.innerHTML = `<p>${cantidad.value} ${divisa1.value} - calcular(divisa1,divisa2) ${divisa2.value} </p>`}
-  
-  // contenedor.innerHTML = "";
-  // for (const money of moneda) {
-  //   let contenedorBody = document.createElement("div");
-  //   //contenedorBody.className = "col-lg-6";
-  //   compro = money.compra;
-  //   contenedorBody.innerHTML = `
-  //   <div class="conversion">
-  //     <div class="conversion-from">
-  //       <p class="conversion-from-text">"Compra "+cotizarD(monto)+" "+money.nombre+"con $"+monto+"\n recuerde agregar los Impuestos sobre la operacion:\n 35% de Percepcion RG4815/20 son $ "+percep(monto)+"\n 30% de Impuesto Pais son $"+imp(monto)"
-  //       </p>
-  //       <!--Compra cotizarD(monto) money.nombre con $monto\n recuerde agregar los Impuestos sobre la operacion:\n 35% de Percepcion RG4815/20 son $percep(monto)\n 30% de Impuesto Pais son $ {imp(monto)}-->
-  //     </div>
-  //   </div>
-  //   <div class="conversion-to">
-  //     <p class="precio"><strong> - ${cotizo(divisa1.value,divisa2.value).toFixed(2)}</strong></p>  
-  //   </div>`;         
-  //   contenedor.append(contenedorBody);
-  // }
-}
+//   // for (const money of moneda) {
+//   //   compro = money.compra;
+//   //   alert("Compra "+cotizarD1(cantidad.value)+" "+money.nombre+" con $"+monto+"\n recuerde agregar los Impuestos sobre la operacion:\n 35% de Percepcion RG4815/20 son $"+percep(cantidad.value)+"\n 30% de Impuesto Pais son $"+imp(cantidad.value));
+//   // }
 
-function cotizarD(monto) {
-     return monto/=compro
-}
-function cotizarP(monto) {
-    return monto*=compro
-}
+  
+//   // const resultado =cantidad*(moneda.find((moneda) => (moneda.nombre == divisa1.value.toUpperCase()))/moneda.find((moneda) => (moneda.nombre == divisa2.value.toUpperCase())))
+//   // monto=cantidad*resultado.compra
+//   // return(`El Signo para ${resultado.nombre} es ${resultado.signo}.`)
+//   // "Compra "+cotizarD(monto)+" "+resultado.nombre+" con $"+monto+"\n recuerde agregar los Impuestos sobre la operacion:\n 35% de Percepcion RG4815/20 son $"+percep(monto)+"\n 30% de Impuesto Pais son $"+imp(monto)
+//   // //             }
+//   //   return monto*=compro
+// }
+
+
 function asignarOperacion(op) {
     if (op == "percepcion") {
         return (monto) => monto*=0.35
